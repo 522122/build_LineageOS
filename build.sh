17248bc713ed9cbbda2231a7413aab3123785c7c
paso <thep450@gmail.com>
2017-01-10 20:10:44 +0100

    initial upload Personal scripts to automate building LineageOS and upload them to my webserver with metadata for working OTA updates.

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+. /home/paso/.bashrc
+
+#define variables
+NEW_BUILD="/home/paso/android/system/out/target/product/gemini/lineage-14.1-`date +%Y%m%d`-UNOFFICIAL-gemini.zip"
+NEW_META="/home/paso/android/system/out/target/product/gemini/meta.txt"
+
+#cleanup
+rm $NEW_BUILD*
+rm $NEW_META
+rm /home/paso/android/system/out/target/product/gemini/*ota*.zip
+
+
+#sync and build
+cd /home/paso/android/system
+repo sync -j8
+. build/envsetup.sh
+brunch gemini
+
+#create metadata for updater
+/home/paso/generatemeta.sh > /home/paso/android/system/out/target/product/gemini/meta.txt
+
+#if main file was successfully created we can upload everything to server
+if [ -f $NEW_BUILD ];
+then
+	scp $NEW_BUILD* pi@192.168.100.2:"/media/USB/www/out/files"
+	scp $NEW_META pi@192.168.100.2:"/media/USB/www/out/api/index.php"
+	echo "Files uploaded to server."
+else
+	echo "Something failed, files not present, please check log."
+fi
+
+
+
---
+++ generatemeta.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+TAR_DATE=`date +%Y%m%d`
+TIMESTAMP=`date -r /home/paso/android/system/out/target/product/gemini/lineage-14.1-$TAR_DATE-UNOFFICIAL-gemini.zip.md5sum +%s`
+INCREMENTAL=`awk -F: '{print $2}' /home/paso/android/system/out/target/product/gemini/build_fingerprint.txt | awk -F\/ '{print $3}'`
+MD5SUM=`awk '{print $1}' /home/paso/android/system/out/target/product/gemini/lineage-14.1-$TAR_DATE-UNOFFICIAL-gemini.zip.md5sum`
+
+echo "{ 
+	id: null,
+	result: [{ 
+		incremental: 'INCREMENTAL', 
+		api_level: '25', 
+		url: 'http://vpn.paskalev.sk/files/lineage-14.1-$TAR_DATE-UNOFFICIAL-gemini.zip',
+		timestamp: $TIMESTAMP, 
+		md5sum: '$MD5SUM', 
+		channel: 'nightly', 
+		filename: 'lineage-14.1-$TAR_DATE-UNOFFICIAL-gemini.zip',
+		changes: '' 
+	}],
+	error: null 
+}"
